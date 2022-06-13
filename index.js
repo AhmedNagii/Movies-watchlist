20c6e94163e990e7ccd41940df344d3583cc2199
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-06-14 01:21:15 +0200

    fetching data
    fetching data and render it as one item only

--- index.js
+++ index.js
@@ -10,5 +10,31 @@ document.getElementById('search').addEventListener('click', ()=>{
     const userInput = document.getElementById('user-input').value
     fetch(`http://www.omdbapi.com/?t=${userInput}&apikey=${apiKey}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+       console.log(data)
+      document.getElementById('movies-container').innerHTML=
+  
+      
+      `<div id="movie">
+      <img src="${data.Poster}" id="movie-poster">
+        <div>
+        <div class="flex-row">
+        <h2 id="movie-title">${data.Title}</h2>
+
+        <p>${data.Ratings[0].Value}⭐</p>
+        </div>
+        <div class="flex-row">
+         <p id="movie-length">${data.Runtime}</p>
+         <p id="movie-type">${data.Genre}</p>
+         <button class="addtolist-btn"><i class="fa fa-plus-square"></i> Watchlist</button>
+        </div>
+        <p id="movie-description">${data.Plot}</p>
+        </div>`
+    })
 })
+
+document.getElementById('movies-container')
+
+
+
+
